e80d6031414a83f0bd140290ab24ff2991433e59
kfclts <kfclts@gmail.com>
2021-07-31 15:23:26 +0800

    Pass data to a parent component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../products';
 
+import { Output, EventEmitter } from '@angular/core';
+
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() productchild!: Product;
+  @Input() productchild!: Product; // Parent(app-product-list) to child(this one)
+
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
